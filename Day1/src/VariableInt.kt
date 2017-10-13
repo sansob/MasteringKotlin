8edf20005c73d7f914b64851d1fdf76674da84d2
Qampus <hasan.shobri@outlook.com>
2017-10-13 20:44:04 +0700

    VariableInt

---
+++ Day1/src/VariableInt.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>){
+    //Int
+    //implicit
+    var age = 31
+    var ageNull = 12
+    //implicit != null
+
+    //explicit
+    var number:Int = 119
+
+    //null Object
+    var numberNull:Int? = null
+    numberNull = 12
+
+    println(age)
+    println(number)
+    println(numberNull)
+
+}
