5bc7a22c53e5509b333788468915a9c2bd397abe
Qampus <hasan.shobri@outlook.com>
2017-10-13 20:12:24 +0700

    Variable

---
+++ Day1/src/Variable.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>){
+    var name = "Hasan"
+    var lastName = "Shobri"
+    var age = 11
+    var weight = 11.5
+
+    //println("Hallo my name is "+ name +" "+lastName + " My age is "+ age + " And my weight are "+ weight)
+    println("Hallo my name is $name $lastName and my age are $age my weight are $weight")
+}
