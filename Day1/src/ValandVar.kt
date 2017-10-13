25c5d8fd49a5bbecd79b0386ff50730815521ed9
Qampus <hasan.shobri@outlook.com>
2017-10-13 21:30:21 +0700

    Different between val and var

---
+++ Day1/src/ValandVar.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>){
+    /*
+    *   Perbedaan antara Val dan Var
+    *   Val itu d gunakan untuk static variable
+    *   Misalkan val pi = 3.14
+    *   Var digunakan untuk variable yang dapat di ubah
+    *   Misalkan var age = 13
+    * */
+
+    val pi = 3.14
+    val name:String = "Hasan"
+    val selNumber = "982jaksjakj9128912819"
+
+
+    var age = 13
+    age = 14
+
+    println("Hai my name is $name, Pi is $pi and my age are $age")
+    println("Serial number are: $selNumber")
+}
