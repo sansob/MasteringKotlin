4c75afbcc0c0ba971970aade1896ffd03e92f430
Qampus <hasan.shobri@outlook.com>
2017-10-13 20:59:59 +0700

    VariableBoolean

---
+++ Day1/src/VariableBoolean.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>){
+    //boolean 2 value, true or false
+
+    //implicit
+    var drink = true
+
+    //explicit
+    var eat:Boolean = false
+
+    //null object
+    var smoke:Boolean? = null
+    //set null object
+    smoke = true
+
+    println("Drink? $drink Eat? $eat , Smoke? $smoke")
+}
