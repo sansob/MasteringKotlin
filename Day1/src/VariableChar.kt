68ebcb4099de107e045a811cfffb4d453dde850e
Qampus <hasan.shobri@outlook.com>
2017-10-13 21:20:22 +0700

    VariableChar

---
+++ Day1/src/VariableChar.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>){
+    //Char = A, B, c, $, D E,
+    //char itu single quotes ('')
+
+    var pungky = 'P'
+
+    var toha:Char? = null
+    toha = '$'
+
+    var name = "Hasan" //String
+
+    println("Hai $pungky , $toha , my name is $name")
+}
