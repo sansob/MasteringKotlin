87bd349f65862952ecc6bf2097d7a88b47cc793c
Qampus <hasan.shobri@outlook.com>
2017-10-13 20:53:39 +0700

    VariableDouble

---
+++ Day1/src/VariableDouble.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>){
+    //Double 11.33 12.09
+
+    //implicits
+    var weight = 13.00
+    //otomatis detect type data
+
+    //explicit tipe data apa?
+    var height:Double = 187.1
+
+    println("Berat badan $weight dan Tinggi $height")
+
+}
