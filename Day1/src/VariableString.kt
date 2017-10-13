5e7d5dad073efe64f501a2872aa7f1e7df246d7b
Qampus <hasan.shobri@outlook.com>
2017-10-13 20:28:51 +0700

    VariableString

---
+++ Day1/src/VariableString.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>){
+
+    //implicit
+
+    var name = "Hasan"
+    var names = null
+    //explicit
+
+    var city:String = "Semarang"
+
+    //variable null
+
+    var country:String? = null
+    country = "Indonesia"
+
+    println(names)
+    println(name)
+    println(city)
+    println(country)
+}
