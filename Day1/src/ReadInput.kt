e7f018638fbedf51ea5d921f9e2791da84052a3e
Qampus <hasan.shobri@outlook.com>
2017-10-13 21:43:31 +0700

    Read User Input

---
+++ Day1/src/ReadInput.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>){
+    /*
+    * User Input
+    */
+
+    println("=======================")
+    println("Welcome to Conv Store")
+    println("=======================")
+
+    println("What can i do for ya? ")
+    var customerResponse = readLine()
+    //println(customerResponse)
+    println("Sure, that's it")
+    println("Any else?")
+    //reading user response
+    var secondCustomerResponse = readLine()
+    //writing to console user response
+    println(secondCustomerResponse)
+    println("Sure")
+}
