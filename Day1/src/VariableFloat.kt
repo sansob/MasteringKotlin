2aab6e1d4dc5f6de8002bf401d5626de1d29a93d
Qampus <hasan.shobri@outlook.com>
2017-10-13 21:12:01 +0700

    VariableFloat

---
+++ Day1/src/VariableFloat.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>){
+    //Float
+    //Float yang bedain sama Double, di kotlin
+    //setiap value Float kasih F d belakang
+    //misal 12.99F
+
+    var price = 12.00
+
+    var discount:Float? = null
+    discount = 13.00F
+    //tanpa F d anggap double
+    println("Price is $price and discount are $discount")
+}
