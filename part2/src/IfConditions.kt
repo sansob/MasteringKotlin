0f025b99c25d621ca13cee2a62a73f30a8e0f0b7
Qampus <hasan.shobri@outlook.com>
2017-10-20 21:13:23 +0700

    IfCondition

---
+++ part2/src/IfConditions.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+    var fNum = 9
+    var lNum = 10
+
+//    if (fNum != lNum){
+//        println("Yeah!")
+//    }
+
+
+    //versi curly
+    if (fNum > lNum) {
+        println("Yes baby")
+    } else {
+        println("NO!")
+    }
+
+    //versi One Line
+    if(fNum < lNum) println("yes of course") else println("Ne")
+}
