60866a899e825ce668db4dfe1e42a018e02b695a
Qampus <hasan.shobri@outlook.com>
2017-10-20 20:18:11 +0700

    Division

---
+++ part2/src/Division.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    var firstNum:Float = 1.0f
+    var lastNum:Float = 10.0f
+    //var udah d def
+    println("Hello, ${firstNum / lastNum}")
+
+    var fNum = 1
+    var lNum = 5
+
+    println("Hello, ${fNum.toFloat() / lNum.toFloat()}")
+    //convert Int ke Float
+}
