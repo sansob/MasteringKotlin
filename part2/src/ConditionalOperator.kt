ef9909aa70ae7ea4d5623016859ee15773e298b3
Qampus <hasan.shobri@outlook.com>
2017-10-20 20:57:07 +0700

    Conditional Operator

---
+++ part2/src/ConditionalOperator.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+    /*
+    * Conditional Operators
+    * ==
+    * <
+    * >
+    * !=
+    * <=
+    * >=
+    * */
+
+    var fNum = 6
+    var lNum = 9
+
+    println(fNum < lNum) //return boolean which mean true or false
+    println(fNum == lNum)
+    println(fNum > lNum)
+    println(fNum != lNum)
+}
