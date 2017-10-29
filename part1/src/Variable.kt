8734f82b3d1013712153624d72ccee91aa208ece
bagus duta prasetya <bagusdutaprasetya@gmail.com>
2017-10-29 15:39:42 +0700

    editing
    tak tambahi var tempat lahir di variable.kt

--- part1/src/Variable.kt
+++ part1/src/Variable.kt
@@ -4,6 +4,9 @@ fun main(args: Array<String>){
     var age = 11
     var weight = 11.5
 
+    //tambahan
+    var ttl = "pati"
+
     //println("Hallo my name is "+ name +" "+lastName + " My age is "+ age + " And my weight are "+ weight)
-    println("Hallo my name is $name $lastName and my age are $age my weight are $weight")
+    println("Hallo my name is $name $lastName i form $ttl and my age are $age my weight are $weight")
 }
